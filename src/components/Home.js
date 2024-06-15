45a3fcbf08e365fe98e52126e78fcc2934614842
Abigail Gillham <abigailg0314@gmail.com>
2024-06-16 00:01:17 +0800

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import './Components.css';
+function Home() {
+    return(
+        <div className = "Top_Title">
+            <header className = "Hello">
+                <h1>Hi! I'm Abby Gillham.</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
